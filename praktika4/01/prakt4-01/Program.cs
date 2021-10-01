662e7aba18b75d4d92d3912deeda3b945662e073
Студент <@DESKTOP-VD4A12V>
2021-10-01 12:21:44 +0300

    fix folder structure

---
+++ praktika4/01/prakt4-01/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+
+namespace prakt4_01
+{
+  class Program
+  {
+
+      static void Main(string[] args)
+      {
+        ArrayList myList = new ArrayList() { "Данила"};
+        myList.Add("выпил");
+        myList.Add("ВАНЮ и ");
+      //myList.Add(0.5);
+        myList.Add("HENESSY");
+  
+        
+      foreach (string item in myList)
+        {
+          Console.WriteLine(String.Format($"Unsorted: {item}") );
+        }
+      //Сортировка при помощи стандартного объекта сравнения
+      myList.Sort();
+
+      //myList.Reverse();
+      foreach (string item in myList)
+      {
+        Console.WriteLine("   Sorted: {0}", item);
+      }
+
+    }
+
+  }
+}
---
+++ praktika4/offTOPSer/offTOPSer/Program.cs
@@ -0,0 +1,42 @@
+﻿using Newtonsoft.Json;
+using System;
+
+namespace offTOPSer
+{
+  [Serializable]
+  class Ezhik
+  {
+    public Ezhik(string name, byte age)
+    {
+      this.name = name;
+      this.age = age;
+    }    
+    public Ezhik()
+    {
+      this.name = "Атем";
+      this.age = 19;
+    }
+
+    public string name { set; get; }
+    public byte age { set; get; }
+
+    public override string ToString()
+    {
+      //return base.ToString();
+      return string.Format($"name: {name}     age: {age}");
+    }
+  }
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Ezhik cip1 = new Ezhik();
+      string json = JsonConvert.SerializeObject(cip1);
+      Console.WriteLine(json);
+      string jsonstring = "{ \"name\":\"Вадим\",\"age\":13}";
+      Ezhik cip2 = new Ezhik();
+      cip2 = JsonConvert.DeserializeObject<Ezhik>(jsonstring);
+      Console.WriteLine(cip2);
+    }
+  }
+}
